4968afaa44947df3a6ed41b49df8c006ecf28ea4
leannetan <lea.tan47@gmail.com>
2020-05-24 16:59:17 +0800

    moved readData method out

--- app/src/main/java/com/example/mainpage/FoodAllActivity.java
+++ app/src/main/java/com/example/mainpage/FoodAllActivity.java
@@ -6,21 +6,12 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.widget.Toast;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-import java.util.Collections;
 
 public class FoodAllActivity extends AppCompatActivity {
 
     RecyclerView rcView;
-    DatabaseReference dbRef;
     FoodList foodList;
     FoodListAdapter adapter;
 
@@ -33,40 +24,13 @@ public class FoodAllActivity extends AppCompatActivity {
         rcView.setHasFixedSize(true);
         rcView.setLayoutManager(new LinearLayoutManager(this));
 
-
-        readData(new FirebaseCallback() {
+        foodList = new FoodList();
+        foodList.readData(new FirebaseCallback() {
             @Override
             public void onCallBack(ArrayList<Food> list) {
                 adapter = new FoodListAdapter(FoodAllActivity.this, list);
                 rcView.setAdapter(adapter);
             }
-        });
+        }, FoodAllActivity.this, FoodList.Category.ALL);
     }
-
-    private void readData(final FirebaseCallback fbCallback) {
-        dbRef = FirebaseDatabase.getInstance().getReference().child("Food");
-        foodList = new FoodList();
-        ValueEventListener vel = new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                for (DataSnapshot dS : dataSnapshot.getChildren()) {
-                    Food f = dS.getValue(Food.class);
-                    foodList.add(f);
-                }
-                fbCallback.onCallBack(foodList.getAll());
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                Toast.makeText(FoodAllActivity.this, "Cancelled", Toast.LENGTH_SHORT).show();
-            }
-        };
-        dbRef.addValueEventListener(vel);
-    }
-
-
-    private interface FirebaseCallback {
-        void onCallBack(ArrayList<Food> list);
-    }
-
 }

2af09c88e2e6f4a4fe55d70b25282578daa74b54
leannetan <lea.tan47@gmail.com>
2020-05-28 05:20:19 +0800

    added libraries info added convenience store button shifted data retrieving into main

---
+++ app/src/main/java/com/example/mainpage/FoodConvenienceActivity.java
@@ -0,0 +1,13 @@
+package com.example.mainpage;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FoodConvenienceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_convenience);
+    }
+}

09634e6bd24eb6158c90ff19692b3506e5b4075b
leannetan <lea.tan47@gmail.com>
2020-06-03 03:33:46 +0800

    change bottom bar and other buttons

--- app/src/main/java/com/example/mainpage/MapActivity.java
+++ app/src/main/java/com/example/mainpage/MapActivity.java
@@ -6,6 +6,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import com.github.chrisbanes.photoview.PhotoView;
